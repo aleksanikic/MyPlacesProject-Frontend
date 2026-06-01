4d0dca2dc465407215dfd7fd6a120dc92056a613
aleksa nikic~ <aleksanikic88@gmail.com>
2026-06-01 18:58:01 +0300

    fix avatar

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -10,7 +10,7 @@ export default function UserItem(props) {
       <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
-            <Avatar image={`${process.env.REACT_APP_ASSET_URL}/${props.image}`} alt={props.name} />
+            <Avatar image={`https://my-places-project-backend.vercel.app/uploads/images/filename.jpg`} alt={props.name} />
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
